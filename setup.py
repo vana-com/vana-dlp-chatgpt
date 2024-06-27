32fffc7fcef5aebf38ae44b51b057b0e9e3b4d71
Tim Nunamaker <tim@vana.com>
2024-06-27 17:42:19 -0500

    CI

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import subprocess
 
 def main():
+    subprocess.run(["poetry", "run", "pre-commit", "autoupdate"], check=True)
     subprocess.run(["poetry", "run", "pre-commit", "install"], check=True)
     print("Project setup complete. Pre-commit hooks installed.")
 
